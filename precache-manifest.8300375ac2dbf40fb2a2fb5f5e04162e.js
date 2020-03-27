de0a9774c8c061d5d8c78c454c15cb5b411d77ea
rafanomezantsoa <mahery.rafanomezantsoa@epitech.eu>
2020-03-27 10:35:37 +0100

    Updates

---
+++ precache-manifest.8300375ac2dbf40fb2a2fb5f5e04162e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc05e1109645b3622188c0843bbb6cb",
+    "url": "/avotravo/index.html"
+  },
+  {
+    "revision": "00b1cd8fc44dd3925bab",
+    "url": "/avotravo/static/css/main.e4ef935c.chunk.css"
+  },
+  {
+    "revision": "56dc1289c9e732780583",
+    "url": "/avotravo/static/js/2.ec505b94.chunk.js"
+  },
+  {
+    "revision": "00b1cd8fc44dd3925bab",
+    "url": "/avotravo/static/js/main.03c7d08f.chunk.js"
+  },
+  {
+    "revision": "ae4b6bd20a11ac1d439a",
+    "url": "/avotravo/static/js/runtime-main.bcdf5955.js"
+  },
+  {
+    "revision": "46ae3f0a215f6c32f5f23655e0608b6a",
+    "url": "/avotravo/static/media/bordeaux.46ae3f0a.jpg"
+  },
+  {
+    "revision": "5610348a58654c783c879432a4889581",
+    "url": "/avotravo/static/media/grenoble.5610348a.jpg"
+  },
+  {
+    "revision": "b538d8c7a09d2ce7be1f804a68a9d9f0",
+    "url": "/avotravo/static/media/lyon.b538d8c7.jpg"
+  },
+  {
+    "revision": "1810cefc081cea24489ea68c2f443a4f",
+    "url": "/avotravo/static/media/paris.1810cefc.jpg"
+  },
+  {
+    "revision": "5595928b7a701a7ce689c35d755ffaf2",
+    "url": "/avotravo/static/media/strasbourg.5595928b.jpg"
+  }
+]);
